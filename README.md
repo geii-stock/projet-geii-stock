6e57d6d967c1c25d2a7ce8951aa7123cc6f8a517
Margaux <margaux.paget@gmail.com>
2015-05-18 10:16:56 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Veille-Techno
+=============
+
+A Symfony project created on May 5, 2015, 1:42 pm.
---
+++ src/GEII/StockBundle/GEIIStockBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GEII\StockBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GEIIStockBundle extends Bundle
+{
+}
