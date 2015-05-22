493743aa1d31d3891f314a4453e30eee2d5c23da
Margaux <margaux.paget@gmail.com>
2015-05-22 10:28:22 +0200

    Modif

--- src/GEII/StockBundle/Form/JournalType.php
+++ src/GEII/StockBundle/Form/JournalType.php
@@ -15,13 +15,28 @@ class JournalType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('prixht')
-            ->add('prixttc')
-            ->add('quantite')
-            ->add('dateha')
+            ->add('idArticle','entity',array(
+                'label' => 'Référence :',
+                'class' => 'GEII\StockBundle\Entity\Article',
+                'property' => 'reference')
+                )
+            ->add('prixht','text',array(
+                'label' => 'Prix d\'achat (HT) :',
+                'attr' => array('placeholder' => 'Exemple: 235.95','size' => 25)
+                ))
+            ->add('prixttc','text',array(
+                'label' => 'Prix d\'achat (TTC) :',
+                'attr' => array('placeholder' => 'Exemple: 5.5','size' => 25)
+                ))
+            ->add('quantite','text',array(
+                'label' => 'Quantité :',
+                'attr' => array('placeholder' => 'Exemple: 123','size' => 25)
+                ))
+            ->add('dateha','text',array(
+                'label' => 'Date d\'achat (+) :'
+                ))
             ->add('dateCreation')
             ->add('createurMouvement')
-            ->add('idArticle')
         ;
     }
     
